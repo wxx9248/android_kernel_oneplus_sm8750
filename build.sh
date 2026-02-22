91ae326f1d4e0b654f1fa78965dcf4f7e5f8e9af
wxx9248 <wxx9248@wxx9248.top>
2026-02-22 12:25:12 -0800

    feat: enable docker support

--- build.sh
+++ build.sh
@@ -19,6 +19,7 @@ DEFCONFIG_FRAGMENTS=(
     arch/arm64/configs/vendor/sun_perf.config
     arch/arm64/configs/vendor/oplus/sun_perf.config
     arch/arm64/configs/kernelsu.config
+    arch/arm64/configs/docker.config
 )
 
 config() {
